619148e8468a1a20094ddca0e483577b5660fe53
Nikita Zhukov <yacuken@gmail.com>
2016-08-27 14:16:07 +0300

    [dcd] fix bluetooth mac address. thnaks adeen-s

--- sparse/usr/bin/droid/droid-hcismd-up.sh
+++ sparse/usr/bin/droid/droid-hcismd-up.sh
@@ -5,10 +5,25 @@
 # not work, so we loop.
 MAXTRIES=15
 
+setprop ro.qualcomm.bt.hci_transport smd
+setprop qcom.bt.dev_power_class 2
+setprop qcom.bt.le_dev_pwr_class 2
+
 seq 1 $MAXTRIES | while read i ; do
+    rfkill unblock all
     echo 1 > /sys/module/hci_smd/parameters/hcismd_set
     if [ -e /sys/class/bluetooth/hci0 ] ; then
+        rfkill unblock all
+        hciconfig hci0 up
         # found hci0, exit successfully
+        echo 0 > /sys/module/hci_smd/parameters/hcismd_set
+        bt_mac=$(/system/bin/hci_qcomm_init -e -p 2 -P 2 -d /dev/ttyHS0 2>1 | grep -oP '([0-9a-f]{2}:){5}([0-9a-f]{2})')
+        echo "BT MAC: $bt_mac"
+        if [ ! -z "$bt_mac" ] ; then
+            echo $bt_mac > /var/lib/bluetooth/board-address
+            echo "BT MAC: $bt_mac"
+        fi
+        echo 1 > /sys/module/hci_smd/parameters/hcismd_set
         exit 0
     fi
     sleep 1
@@ -17,4 +32,3 @@ seq 1 $MAXTRIES | while read i ; do
         exit 1
     fi
 done
-
